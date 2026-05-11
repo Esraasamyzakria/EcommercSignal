c21bd118886a2c925d455e2168b0f06a66cbbce9
Esraasamyzakria <esamy3955@gmail.com>
2026-05-11 18:56:50 +0300

    fix

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,29 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  // 🔹 Dynamic routes → Server (SSR)
+  {
+    path: 'details/:id/:slug',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'checkout/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'category-details/:id/:slug',
+    renderMode: RenderMode.Server
+  },
+
+  // 🔹 Static pages فقط
+  {
+    path: '',
+    renderMode: RenderMode.Prerender
+  },
+
+  // 🔹 fallback (مهم جدًا)
   {
     path: '**',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Server
   }
 ];
