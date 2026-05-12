317845a44ee6934d54875b2815dd3e83130ada13
Esraasamyzakria <esamy3955@gmail.com>
2026-05-12 20:51:53 +0300

    chanage

--- src/app/features/details/details.component.ts
+++ src/app/features/details/details.component.ts
@@ -2,6 +2,8 @@ import { Component, CUSTOM_ELEMENTS_SCHEMA, inject, OnInit, signal } from '@angu
 import { ActivatedRoute } from '@angular/router';
 import { ProductService } from '../../core/services/product/product.service';
 import { Product } from '../../core/models/product.interface';
+import { CartService } from '../../core/services/cart/cart.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-details',
@@ -12,6 +14,8 @@ import { Product } from '../../core/models/product.interface';
 })
 export class DetailsComponent implements OnInit {
   _activatedRoute=inject(ActivatedRoute);
+    _toastr=inject(ToastrService)
+  _cartservices=inject(CartService)
   _productService=inject(ProductService);
   productDetails=signal<Product>({} as Product);
   ngOnInit(): void {
@@ -31,4 +35,21 @@ export class DetailsComponent implements OnInit {
     }
   })
   }
+    addProductToCart(prodId: string) {
+  if (localStorage.getItem('token')) {
+    this._cartservices.addProductToCart(prodId).subscribe({
+      next: (res) => {
+ if(res.status==="success"){
+  this._cartservices.cartCount.set(res.numOfCartItems)
+    this._toastr.success(res.message, 'Success', { progressBar: true, closeButton: true });
+ }
+       
+      },
+    });
+  } 
+  else {
+    this._toastr.warning('Please login first', 'Warning', { progressBar: true, closeButton: true });
+  }
+
+}
 }
